01c5a391c6e395fbbb027c634c2a3be8d21c88b5
smccoy709 <scottymccoyjob@gmail.com>
2023-03-09 08:52:47 -0500

    commit 2

--- config/database.php
+++ config/database.php
@@ -15,18 +15,20 @@
 			$this->port = getenv('PORT');
 		}
 	
-		public function connect() {
-			$this->conn = null;
-			
-			$dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
-			
-			try {
-				$this->conn = new PDO($dsn, $this->username, $this->password);
-			} catch (PDOException $e) {
-				echo "Connection Error:  " . $e->getMessage();
-			} 
-			
-			return $this->conn;
+		public function connect () {
+			if ($this->conn) {
+				return $this->conn;
+			} else {
+				$dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+
+				try {
+					$this->conn = new PDO($dsn, $this->username, $this->password);
+					$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+					return $this->conn;
+				} catch(PDOException $e) {
+					echo 'Connection Error: ' . $e->getMessage();
+				}
+			}
 		}
 	}
 ?>
