3004558278853fee827d9452abf80bd95d941680
smccoy709 <scottymccoyjob@gmail.com>
2023-03-09 12:30:56 -0500

    sss

--- api/authors/index.php
+++ api/authors/index.php
@@ -1,7 +1,4 @@
-<?php
-    require('../../config/database.php');
-    require('../../model/Author.php');
-    
+<?php    
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     $method = $_SERVER['REQUEST_METHOD'];
