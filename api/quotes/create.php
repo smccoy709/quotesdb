c5c875a2e2dd312833f8c79b313bba89977aea39
smccoy709 <scottymccoyjob@gmail.com>
2023-03-09 14:56:31 -0500

    dd

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -14,16 +14,30 @@
 		
 	$data = json_decode(file_get_contents("php://input"));
 	$quotes->quote = $data->quote;
-	$quotes->author_id = $data->author_id;
+
+	if ($quotes->author_id !== null) {
+		echo json_encode(
+			$quotes->author_id = $data->author_id;
+			if ($quotes->create()) {
+				echo json_encode(
+					array('message' => 'Quote Created')
+				);
+			} else {
+				echo json_encode(
+					array('message' => 'Quote Not Created')
+				);
+			}
+		} else {
+			echo json_encode(
+				array('message' => 'category_id Not Found')
+		);
+	}
+	
 	$quotes->category_id = $data->category_id;
 	
 	if ($quotes->create()) {
 		echo json_encode(
 			array('message' => 'Quote Created')
-		);
-	} else if ($quotes->category_id === null) {
-		echo json_encode(
-			array('message' => 'category_id Not Found')
 		);
 	} else {
 		echo json_encode(
