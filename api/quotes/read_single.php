8480816fd2029184de616b35d2c9d1a86c84ab16
smccoy709 <scottymccoyjob@gmail.com>
2023-03-10 14:39:49 -0500

    s

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -31,10 +31,9 @@
 		}
 	}
 	
-	if (isset($_GET['author_id']) !== null) {
+	if (isset($_GET['author_id'])) {
 		$quotes->id = isset($_GET['author_id']) ? $_GET['author_id'] : die();
 		$quotes_arr = $quotes->read_single();
-		echo json_encode($quotes_arr);
 	}
 	
 	if (isset($_GET['category_id'])) {
