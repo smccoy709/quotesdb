df43edec036358869bd90d19890518d4569ac537
smccoy709 <scottymccoyjob@gmail.com>
2023-03-10 09:23:12 -0500

    bhb

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -26,7 +26,27 @@
 	$quotes->author_id = $data->author_id;
 	$quotes->category_id = $data->category_id;
 	
-	if($quotes->update()) {
+	if (isset($_GET['id'])) {
+		$quotes->id = isset($_GET['id']) ? $_GET['id'] : die();
+		$quotes->update();
+	
+		$quotes_arr = array(
+			'id' => $quotes->id,
+			'quote' => $quotes->quote,
+			'author' => $quotes->author,
+			'category' => $quotes->category
+		);
+
+		if($quotes->quote !== null) {
+			//Change to JSON data
+			print_r(json_encode($quotes_arr, JSON_NUMERIC_CHECK));
+		} else {
+			echo json_encode(
+				array('message' => 'No Quotes Found')
+			);
+		}
+	}
+	/*if($quotes->update()) {
 		echo json_encode(
 			array('id'=>$quotes->id, 'quote'=>$quotes->quote, 'author_id'=>$quotes->author_id, 'category_id'=>$quotes->category_id)
 		);
@@ -34,5 +54,5 @@
 		echo json_encode(
 			array('message' => 'No Quotes Found')
 		);
-	}
+	}*/
 ?>
