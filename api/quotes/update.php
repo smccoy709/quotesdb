d99fa525ea03c553a60e5ce9104ac98ced59106e
smccoy709 <scottymccoyjob@gmail.com>
2023-03-10 08:44:29 -0500

    dd

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -17,13 +17,6 @@
 	$quotes->quote = $data->quote;
 	$quotes->author_id = $data->author_id;
 	$quotes->category_id = $data->category_id;
-
-	if(!isset($data->id) or !isset($data->quote) or !isset($data->author_id) or !isset($data->category_id)) {
-		echo json_encode(
-			array('message' => 'Missing Required Parameters')
-		);
-		exit();
-	}
 	
 	if($quotes->update()) {
 		echo json_encode(
@@ -33,5 +26,12 @@
 		echo json_encode(
 			array('message' => 'No Quotes Found')
 		);
+	}
+
+	if(!isset($data->id) || !isset($data->quote) || !isset($data->author_id) || !isset($data->category_id)) {
+		echo json_encode(
+			array('message' => 'Missing Required Parameters')
+		);
+		exit();
 	}
 ?>
