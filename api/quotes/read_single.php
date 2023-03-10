d77c3a94af3c5d57d2b4e41e5e7c4679ccda81e9
smccoy709 <scottymccoyjob@gmail.com>
2023-03-10 14:20:34 -0500

    a

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -37,13 +37,13 @@
 	}
 	
 	if (isset($_GET['category_id'])) {
-		$quotes->id = isset($_GET['category_id']) ? $_GET['category_id'] : die();
+		$quotes->category_id = isset($_GET['category_id']) ? $_GET['category_id'] : die();
 		$quotes_arr = $quotes->read_single();
 	}
 	
 	if (isset($_GET['author_id']) && isset($_GET['category_id'])) {
 		$quotes->id = isset($_GET['author_id']) ? $_GET['author_id'] : die();
-		$quotes_arr = $quotes->read_single();
+		$quotes->read_single();
 	}
 
 	echo json_encode($quotes_arr);
